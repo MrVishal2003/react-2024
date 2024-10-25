f2a9a98bacb0e1c873e2e959cd48fa19c3fa1616
MrVishal2003 <hadiyal123vvv@gmail.com>
2024-10-25 21:31:22 +0530

    done

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,13 @@
+import Chai from "./Chai";
+
 function App() {
   return (
+    <>
+    <Chai/>
     <h1>chai aur react | Vishal</h1>
+    </>
   );
 }
 
 export default App;
+
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,12 @@
+import Chai from "./chai"
 function App() {
   
   return (
-    <h1>chai aur react with vite | vishal</h1>
+    <>
+      <Chai/>
+      <h1>chai aur react</h1>
+      <p>complete vite</p>
+    </>
   )
 }
 
