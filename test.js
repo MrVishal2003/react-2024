1f643f8dcae0418fadfc621bcb04fb013c8f330e
MrVishal2003 <hadiyal123vvv@gmail.com>
2024-12-13 16:08:11 +0530

    done

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
-console.log(null + 1);
+// let arr = [1,2,3,4,5];
 
-let a = 1;
-let b = a++;
-console.log(a, b);
+// let result = arr
+//     .filter((num) => num % 2!== 0)
+//     .map((num) => num ** 2)
+//     .reduce((sum, num) => sum + num, 0);
+
+// console.log(result);
+
+let X = 10;
+
+function outer () {
+    console.log(X);
+    if (false) {
+        var X = 20; // if we use let to declare variavle those answer is 10.
+    }
+}
+outer();
